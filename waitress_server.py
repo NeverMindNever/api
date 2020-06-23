f5527b58cd088a288a0c5bce34b247b5c9ec0641
A. T <alaa@live.cl>
2020-06-23 17:22:12 +0200

    Change the name from api to app

--- waitress_server.py
+++ waitress_server.py
@@ -1,6 +1,6 @@
 from waitress import serve
-import api
+import app
 import logging
 logger = logging.getLogger('waitress')
 logger.setLevel(logging.INFO)
-serve(api.app, host='0.0.0.0', port=8081)
+serve(app.app, host='0.0.0.0', port=8081)
