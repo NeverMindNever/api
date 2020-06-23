561605850db2b68b443ec42a51f2e3dc596fbb27
A. T <alaa@live.cl>
2020-06-23 17:42:35 +0200

    Change port to default 8080 and requirements.txt

--- waitress_server.py
+++ waitress_server.py
@@ -3,4 +3,4 @@ import app
 import logging
 logger = logging.getLogger('waitress')
 logger.setLevel(logging.INFO)
-serve(app.app, host='0.0.0.0', port=8081)
+serve(app.app, host='0.0.0.0', port=8080)
